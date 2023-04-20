ce6164e159d5590b3d3f860bca80e49097bbef6c
wilfredofficial63854 <131396513+wilfredofficial63854@users.noreply.github.com>
2023-04-20 14:47:45 -0700

    update to product-details.php

--- product-details.php
+++ product-details.php
@@ -115,7 +115,7 @@
 									Address: F65A-Stadium Residential Area, Tumu,Upper West-Ghana
 								</p>
 								<p> Phone: +233 20 114 4293</p>
-								<p> Email: info@domain.com </p>
+								<p> Email: info@agriconnect.com </p>
 							</div>
 							
 							<div class="useful-links">
